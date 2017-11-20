044992127f637c102e07bf3cb746ddb5bfe32e3a
Ameet Vadhia <ameet01@gmail.com>
2017-11-19 19:19:14 -0800

    Add nested routes for user's channels and channel's messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   root 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:show, :index, :create, :update]
+    resources :users, only: [:show, :index, :create, :update] do
+      resources :channels, only: [:index]
+    end
     resource :session, only: [:new, :create, :destroy]
-    resources :channels, only: [:create, :index, :show, :destroy, :update]
+    resources :channels, only: [:create, :index, :show, :destroy, :update] do
+      resources :messages, only: [:index]
+      resources :users, only: [:index]
+    end
     resources :messages, only: [:show, :index, :create, :update, :destroy]
   end
 end
