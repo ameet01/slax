e17b283a274c896ef84014edb599c1bab3bf063b
Ameet Vadhia <ameet01@gmail.com>
2017-11-19 19:36:28 -0800

    Setup react frontend structure

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      Hi
+    </div>
+  );
+};
+
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,11 @@
-
-
+import React from 'react';
+import configureStore from './store/store';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  const root = document.getElementById('root');
+  const store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
 });
