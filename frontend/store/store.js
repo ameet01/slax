e95ddf77b8af84d036c6b1d8f733791016cb3dc9
Ameet Vadhia <ameet01@gmail.com>
2017-11-20 17:13:26 -0800

    remove redux dev tools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,13 +2,12 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = (preloadedState={}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger)));
+    applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
