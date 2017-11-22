4fdd8dcd7c7f59fbff3b5a63b942d84df370fbc1
Ameet Vadhia <ameet01@gmail.com>
2017-11-21 22:31:34 -0800

    Minor bug fix

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Main = ({logout, currentUser}) => {
   var button;
-  
+
   if(currentUser) {
     button = <button onClick={() => logout()}>Log Out</button>;
   }
 
   return <div>
-      <h1>Hi {currentUser.username.toUpperCase()}!</h1>
+      <h1>Hi {currentUser.username}!</h1>
       {button}
   </div>;
 };
