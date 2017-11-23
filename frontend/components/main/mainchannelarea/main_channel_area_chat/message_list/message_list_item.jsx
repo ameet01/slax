ef682739ac88682417635dc90448d5f5f49219c4
Ameet Vadhia <ameet01@gmail.com>
2017-11-23 09:40:28 -0800

    Make structure for messages

--- frontend/components/main/mainchannelarea/main_channel_area_chat/message_list/message_list_item.jsx
+++ frontend/components/main/mainchannelarea/main_channel_area_chat/message_list/message_list_item.jsx
@@ -8,11 +8,15 @@ class MessageListItem extends React.Component {
   render() {
     return (
       <section className='message-list-item'>
+        <img src ='https://rlv.zcache.com/iron_man_assemble_square_sticker-r5962aceb77654e31b7642af0558ebbce_v9i40_8byvr_324.jpg' className='user-message-picture'></img>
+
         <div className='message-content-header'>
-          <span className='message-content-header-user'>{this.props.message.user.username}</span>: {this.props.message.created_at}
-        </div>
-        <div className='message-body'>
-          {this.props.message.body}
+          <div>
+            <span className='message-content-header-user'>{this.props.message.user.username}</span>: <span className='message-content-header-timestamp'>{this.props.message.created_at}</span>
+          </div>
+          <div className='message-body'>
+            {this.props.message.body}
+          </div>
         </div>
       </section>
     );
