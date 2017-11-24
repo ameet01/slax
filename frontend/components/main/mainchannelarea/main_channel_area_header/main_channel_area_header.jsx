f71d3b3aa0ea3b1b2b3aafcff386671bc07d017b
Ameet Vadhia <ameet01@gmail.com>
2017-11-24 15:54:34 -0800

    minor changes

--- frontend/components/main/mainchannelarea/main_channel_area_header/main_channel_area_header.jsx
+++ frontend/components/main/mainchannelarea/main_channel_area_header/main_channel_area_header.jsx
@@ -17,7 +17,7 @@ class MainChannelAreaHeader extends React.Component {
       headerTitle = <div className='main-header-channel'>#{this.props.channel.name} </div>;
       userCount = <div className='main-header-usercount'><i class="fa fa-user-o" aria-hidden="true"></i><span>{this.props.channel.userCount}</span></div>
     } else {
-      headerTitle = <div className='main-header-channel'>#{this.props.channel.users.filter(user => this.props.currentUser.username !== user.username).map(user => user.username).join(', ')} </div>;
+      headerTitle = <div className='main-header-channel'>{this.props.channel.users.filter(user => this.props.currentUser.username !== user.username).map(user => user.username).join(', ')} </div>;
       if(this.props.channel.userCount > 2) {
         userCount = <div className='main-header-usercount'><i class="fa fa-user-o" aria-hidden="true"></i><span>{this.props.channel.userCount}</span></div>
       } else {
